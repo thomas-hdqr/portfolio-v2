9ec4d4742598aee8b92412550ece0f0905c51485
Thomas <thomashaudiquer@gmail.com>
2022-10-19 16:54:57 +0100

    links

--- components/Aboutme.js
+++ components/Aboutme.js
@@ -1,15 +1,45 @@
 import Image from 'next/image'
+import { useAnimation, motion } from "framer-motion";
+import React, { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
 function Aboutme() {
+
+  // animation for my photo
+  const imageVariants = {
+    visible: { opacity: 1, transition: {ease: "easeOut", duration: 1 }, x: 0, y: 0, rotate: 6 },
+    hidden: { opacity: 0, x: 300, y: 250 }
+  };
+
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+  useEffect(() => {
+      if (inView) {
+        controls.start("visible");
+      }
+    }, [controls, inView]);
+
+
   return (
-    <div id="about">
+    <div id="about" className="p-40">
+      <div className='flex place-content-evenly align-baseline'>
         <div>
-            <p>Text 1</p>
-            <p>Text 2</p>
+            <p className="text-3xl pb-10">Thomas Haudiquer</p>
+            <p className="text-3xl">I am a front-end developer based in Paris, France. I work on different projects to make the web a better place.</p>
         </div>
-        <div>
-            <Image />
+        <div className="ml-32">
+          <p>My expertise is creativity, simplicity and sense for animations. My passion for web development started 2 years ago</p>
         </div>
+      </div>
+        <motion.div
+          ref={ref}
+          animate={controls}
+          initial="hidden"
+          variants={imageVariants}
+          className='text-center py-15'
+        >
+            <Image src="/polaroid-low-resolution.png" alt="Thomas Haudiquer" width="400" height="400"/>
+        </motion.div>
     </div>
   )
 }
--- components/Contact.js
+++ components/Contact.js
@@ -2,13 +2,24 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div id="contact">
-        <div>
-            <h2>Want to work together?</h2>
-            <p>thomashaudiquer@gmail.com</p>
+    <div id="contact" className="px-20 h-screen pt-20">
+        <div className="">
+            <h2 className="text-7xl">Want to work together?</h2>
+            <div className="pt-20">
+              <a className="text-9xl break-words" href="mailto:thomashaudiquer@gmail.com">thomashaudiquer@<br/>gmail.com</a>
+            </div>
         </div>
-        <div>
-            <p><a href="https://github.com/">linkedin</a></p>
+        <div className="footer-menu flex flex-row justify-center pt-48">
+            <p><a href="https://github.com/" target="_blank" rel="noopener noreferrer">linkedin</a></p>
+            <p>,</p>
+            &nbsp;
+            <p><a href="https://github.com/" target="_blank" rel="noopener noreferrer">twitter</a></p>
+            <p>,</p>
+            &nbsp;
+            <p><a href="https://github.com/" target="_blank" rel="noopener noreferrer">github</a></p>
+            <p>,</p>
+            &nbsp;
+            <p><a href="https://github.com/" target="_blank" rel="noopener noreferrer">codepen</a></p>
         </div>
     </div>
   )
