d2d281c5fa8323a6c9a410a5f3a4bdfbd24b42f2
Thomas <thomashaudiquer@gmail.com>
2022-10-21 10:09:55 +0100

    commit

---
+++ pages/links.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+function Links() {
+  return (
+    <div>
+        <h1 className="">Ceci est un test</h1>
+    </div>
+  )
+}
+
+export default Links
