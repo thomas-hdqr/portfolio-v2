633188f8c13e1602ad336725b5d8f48c209e8d3f
Thomas <thomashaudiquer@gmail.com>
2022-10-21 11:15:35 +0100

    commit

--- pages/links.js
+++ pages/links.js
@@ -3,14 +3,14 @@ import Image from 'next/image'
 
 function Links() {
   return (
-    <div className="text-center sm:p-80 bg-yellow-400">
+    <div className="text-center sm:px-40 sm:py-20 py-5 h-screen bg-yellow-400">
 
         {/* Profile */}
         <div className="overflow-hidden rounded-2xl">
             <Image src="/polaroid-low-resolution.png" alt="Thomas Haudiquer" width="200" height="200"className="rounded-full max-w-full h-auto" />
         </div>
-        <h1 className="text-5xl font-bold">Thomas Haudiquer</h1>
-        <h2 className="text-3xl font-normal">Frontend developer</h2>
+        <h1 className="sm:text-5xl text-2xl font-bold">Thomas Haudiquer</h1>
+        <h2 className="sm:text-3xl text-xl font-normal">Frontend developer</h2>
 
 
         {/* Contact icons */}
@@ -40,6 +40,15 @@ function Links() {
 
         {/* Links */}
         <div className="">
+            <div className="bg-red-500 mx-20 my-6 py-4 cursor-pointer border-2 border-black hover:scale-105 transform transition duration-300 ease-out">
+                 <p className="text-xl font-bold">Buy Me A Coffee ☕️</p>
+            </div>
+            <div className="bg-red-500 mx-20 my-6 py-4 cursor-pointer border-2 border-black hover:scale-105 transform transition duration-300 ease-out">
+                 <p className="text-xl font-bold">Buy Me A Coffee ☕️</p>
+            </div>
+            <div className="bg-red-500 mx-20 my-6 py-4 cursor-pointer border-2 border-black hover:scale-105 transform transition duration-300 ease-out">
+                 <p className="text-xl font-bold">Buy Me A Coffee ☕️</p>
+            </div>
             <div className="bg-red-500 mx-20 my-6 py-4 cursor-pointer border-2 border-black hover:scale-105 transform transition duration-300 ease-out">
                  <p className="text-xl font-bold">Buy Me A Coffee ☕️</p>
             </div>
