ccdcac713d853eba9c67901967bead51d336f9fb
Thomas <thomashaudiquer@gmail.com>
2022-10-29 17:03:08 +0100

    commit

--- components/Projectcard.js
+++ components/Projectcard.js
@@ -18,19 +18,21 @@ function Projectcard(props) {
 
   return (
     <div 
-    className="flex flex-col min-h-[640px] h-full w-[443px] border-2 border-black flex-none relative scrollbar-hide"
+    className="flex flex-col justify-between min-h-[640px] h-full w-[443px] border-2 border-black flex-none relative scrollbar-hide"
     onMouseOver={handleMouseOver} 
     onMouseOut={handleMouseOut} 
     onClick={() => {props.project.link}}
     >
       {isHovering && 
-        <Image src={props.project.image} alt={props.project.title} layout="fill" objectFit='cover'  /> 
+        <Image src={props.project.image} alt={props.project.title} layout="fill" objectFit='contain'  /> 
       }
       <p className="text-3xl p-10">{props.project.title}</p>
-      <p className="pt-10 px-10">{props.project.description}</p>
-      <p className="px-10 text-base">{props.project.tech}</p>
-      <div className="cursor-pointer border-2 border-black rounded-2xl m-10">
-          <a href={props.project.link} target="_blank" rel="noopener noreferrer"><p className="text-center p-2">See project</p></a> 
+      <div>
+        <p className="pt-10 px-10">{props.project.description}</p>
+        <p className="px-10 text-base">{props.project.tech}</p>
+      </div>
+      <div className="cursor-pointer border-2 border-black rounded-2xl m-10 hover:bg-black">
+          <a href={props.project.link} target="_blank" rel="noopener noreferrer"><p className="text-center p-2 hover:text-white">See project</p></a> 
       </div>
     </div>
   )
