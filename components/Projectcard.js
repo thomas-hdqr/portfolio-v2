5f1a363a2fc103d03861692746a3de43d3accbc4
Thomas <thomashaudiquer@gmail.com>
2022-10-29 16:38:50 +0100

    improving work cards

--- components/Projectcard.js
+++ components/Projectcard.js
@@ -23,10 +23,12 @@ function Projectcard(props) {
     onMouseOut={handleMouseOut} 
     onClick={() => {props.project.link}}
     >
-      {isHovering && <Image src={props.project.image} alt={props.project.title} layout="fill" objectFit='cover'  /> }
-      <p className="">{props.project.title}</p>
-      <p className="">{props.project.description}</p>
-      <p className="">{props.project.tech}</p>
+      {isHovering && 
+        <Image src={props.project.image} alt={props.project.title} layout="fill" objectFit='cover'  /> 
+      }
+      <p className="text-3xl p-10">{props.project.title}</p>
+      <p className="pt-10 px-10">{props.project.description}</p>
+      <p className="px-10 text-base">{props.project.tech}</p>
       <div className="cursor-pointer border-2 border-black rounded-2xl m-10">
           <a href={props.project.link} target="_blank" rel="noopener noreferrer"><p className="text-center p-2">See project</p></a> 
       </div>
