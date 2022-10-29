50bb30c25da94526b978a4aa73fad244aaf0a67a
Thomas <thomashaudiquer@gmail.com>
2022-10-29 16:00:49 +0100

    commit

--- components/Projectcard.js
+++ components/Projectcard.js
@@ -2,15 +2,15 @@ import Image from 'next/image'
 
 function Projectcard(props) {
   return (
-    <div className="w-[443px] border-2 border-black flex-none relative scrollbar-hide">
-    <Image src={props.project.image} alt={props.project.title} layout="fill" objectFit='cover'  />
-    <p className="">{props.project.title}</p>
-    <p className="">{props.project.description}</p>
-    <p className="">{props.project.tech}</p>
-    <div className="cursor-pointer border-2 border-black rounded-2xl m-10">
-        <a href={props.project.link} target="_blank" rel="noopener noreferrer"><p className="text-center p-2">See project</p></a> 
+    <div className="min-h-[640px] w-[443px] border-2 border-black flex-none relative scrollbar-hide">
+      <Image src={props.project.image} alt={props.project.title} layout="fill" objectFit='cover'  />
+      <p className="">{props.project.title}</p>
+      <p className="">{props.project.description}</p>
+      <p className="">{props.project.tech}</p>
+      <div className="cursor-pointer border-2 border-black rounded-2xl m-10">
+          <a href={props.project.link} target="_blank" rel="noopener noreferrer"><p className="text-center p-2">See project</p></a> 
+      </div>
     </div>
-</div>
   )
 }
 
