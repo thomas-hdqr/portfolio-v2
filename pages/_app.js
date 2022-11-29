4f0cf21f53ae926fd49b2f64d1fdae0b23fbf59a
Thomas <thomashaudiquer@gmail.com>
2022-11-29 15:03:25 +0000

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
+/* eslint-disable @next/next/no-sync-scripts */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import '../styles/globals.css'
 import {ThemeProvider} from 'next-themes'
 
 
 function MyApp({ Component, pageProps }) {
   return (
+      
       <ThemeProvider enableSystem={true} attribute="class">
         <Component {...pageProps} />
       </ThemeProvider>
