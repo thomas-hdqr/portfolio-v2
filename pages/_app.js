5683418604851ffc3fc732715da3430d0073798e
Thomas <thomashaudiquer@gmail.com>
2022-11-29 15:09:09 +0000

    google analytics tracking added

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,26 @@
-/* eslint-disable @next/next/no-sync-scripts */
-/* eslint-disable react/jsx-no-comment-textnodes */
 import '../styles/globals.css'
 import {ThemeProvider} from 'next-themes'
+import Script from 'next/script'
 
 
 function MyApp({ Component, pageProps }) {
   return (
-      
-      <ThemeProvider enableSystem={true} attribute="class">
+    <ThemeProvider enableSystem={true} attribute="class">
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-G-H4MB4WY3C5"/>
+        <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+        __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-H4MB4WY3C5', {
+        page_path: window.location.pathname,
+        });
+        `,
+        }}
+        />
         <Component {...pageProps} />
       </ThemeProvider>
   )
