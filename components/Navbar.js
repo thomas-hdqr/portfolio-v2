5e1aef6b5d331f85fad70118cc187598fc5217e0
Thomas <thomashaudiquer@gmail.com>
2023-06-08 15:25:26 +0100

    commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,35 +1,4 @@
-import { useTheme } from 'next-themes'
-import { MoonIcon, SunIcon } from '@heroicons/react/24/solid'
-import { useState, useEffect } from 'react'
-
 function Navbar() {
-
-  const {systemTheme, theme, setTheme} = useTheme()
-  const [mounted, setMounted] = useState(false)
-
-  useEffect(() => {
-    setMounted(true)
-  },[])
-
-  const renderThemeChanger = () => {
-    if (!mounted) return null;
-
-    const currentTheme = theme === 'system' ? systemTheme : theme;
-
-    if (currentTheme === 'dark') {
-      return (
-        <SunIcon className='w-5 h-5' role="button" onClick={() => setTheme
-        ('light')} />
-      )
-    }
-    else {
-      return (
-        <MoonIcon className='w-5 h-5' role="button" onClick={() => setTheme
-        ('dark')} />
-      )
-    }
-  }
-
 
   return (
     <div className="hidden sm:flex flex-row justify-between sticky">
@@ -45,7 +14,6 @@ function Navbar() {
             &nbsp;
             <p><a href="#contact">contact</a></p>
             <div className="ml-5 self-center">
-              {renderThemeChanger()}
             </div>
         </div>
     </div>
