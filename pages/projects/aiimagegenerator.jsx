7faad449cff309562aa0883716a429be1ec94751
Thomas <thomashaudiquer@gmail.com>
2023-06-16 11:52:01 +0100

    commit

--- pages/projects/aiimagegenerator.jsx
+++ pages/projects/aiimagegenerator.jsx
@@ -4,42 +4,42 @@ import { motion, useScroll, useTransform } from "framer-motion";
 const transition = { duration: 1.4, ease: [0.6, 0.01, -0.05, 0.9] };
 
 const firstName = {
-  initial: {
-    y: 0,
-  },
-  animate: {
-    y: 0,
-    transition: {
-      delayChildren: 0.6,
-      staggerChildren: 0.2,
-      staggerDirection: -1,
+    initial: {
+      y: 0,
     },
-  },
-};
-
-const lastName = {
-  initial: {
-    y: 0,
-  },
-  animate: {
-    y: 0,
-    transition: {
-      delayChildren: 0.6,
-      staggerChildren: 0.04,
-      staggerDirection: 1,
+    animate: {
+      y: 0,
+      transition: {
+        delayChildren: 0.6,
+        staggerChildren: 0.04,
+        staggerDirection: -1,
+      },
     },
-  },
-};
-
-const letter = {
-  initial: {
-    y: 400,
-  },
-  animate: {
-    y: 50,
-    transition: { duration: 2, ...transition },
-  },
-};
+  };
+  
+  const lastName = {
+    initial: {
+      y: 0,
+    },
+    animate: {
+      y: 0,
+      transition: {
+        delayChildren: 0.6,
+        staggerChildren: 0.04,
+        staggerDirection: 1,
+      },
+    },
+  };
+  
+  const letter = {
+    initial: {
+      y: 400,
+    },
+    animate: {
+      y: 0,
+      transition: { duration: 2, ...transition },
+    },
+  };
 
 const AiImageGenerator = ({ imageDetails: propImageDetails }) => {
   const imageDetails = {
